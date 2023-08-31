fc4a8a7a4a1420a2cdaf9549d2909fb88c65c9d6
wassef911 <1.wassef911@gmail.com>
2023-08-31 19:10:33 +0100

    feat: better exceptions

--- src/env_should_be/main.py
+++ src/env_should_be/main.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 
 import argparse
 from dataclasses import dataclass
+from json.decoder import JSONDecodeError
 
+from .exception import DescriptionFileNotLoading
+from .exception import EnvironmentFileNotLoading
 from .utils import *
 
 
@@ -46,20 +49,39 @@ if __name__ == '__main__':
         default=None,
     )
     args: Namespace = parser.parse_args()
-    env = load_env_file(
-        args.env_file) if args.env_file != None else load_all_env_vars()
+    if args.env_file:
+        try:
+            env = load_env_file(args.env_file)
+        except (
+            FileNotFoundError,
+            ValueError,
+            JSONDecodeError,
+        ) as exc:
+            raise EnvironmentFileNotLoading(
+                f"couldn't load file at:{args.env_file}, {exc}"
+            )
+    else:
+        env = load_all_env_vars()
     errors: list[EnvironmentError] = []
     for path in args.description:
-        is_valid = is_valid_env(load_json_file(path), env)
+        try:
+            description: dict = load_json_file(path)
+        except (
+            FileNotFoundError,
+            ValueError,
+            JSONDecodeError,
+        ) as exc:
+            raise DescriptionFileNotLoading(
+                f"couldn't load file at:{path}, {exc}")
+        is_valid = is_valid_env(description, env)
         if is_valid != True:
             errors.append(EnvironmentError(
                 description_path=path, errors=is_valid))
 
     if errors.__len__() and not args.fail_silently:
         for e in errors:
-            print('\n')
             print(f'Env Not matching {e.description_path}')
             for variable, fails in e.errors:
-                print(f'\n \n {variable}, failing to match {fails}')
+                print(f'\n {variable}, failing to match {fails}')
         exit(1)
     exit(0)
