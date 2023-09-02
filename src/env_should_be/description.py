0a6c7b27cf33f04c4efb30fe2932d9478871d69a
wassef911 <1.wassef911@gmail.com>
2023-09-02 16:11:39 +0100

    feat: load yml descriptions

--- src/env_should_be/description.py
+++ src/env_should_be/description.py
@@ -1,39 +1,39 @@
 from __future__ import annotations
 
 __all__ = (
-    'Boolean',
-    'Length',
-    'MinLength',
-    'MaxLength',
-    'Regex',
-    'Option',
-    'Constant',
-    'IsInt',
-    'IsStr',
-    'IsFloat',
+    "Boolean",
+    "Length",
+    "MinLength",
+    "MaxLength",
+    "Regex",
+    "Option",
+    "Constant",
+    "IsInt",
+    "IsStr",
+    "IsFloat",
 )
 
 import re
 from abc import ABC
 from abc import abstractmethod
-from typing import Any
+from typing import Optional
 from .exception import ValueUnassignableToDescription
 
 
 class Description(ABC):
-    def __init__(self, value: Any):
+    def __init__(self, value):
         self.value = value
 
     @abstractmethod
-    def is_valid(self, value: Any) -> bool:
+    def is_valid(self, value) -> bool:
         pass
 
     @property
-    def value(self) -> Any:
+    def value(self):
         return self.__value
 
     @value.setter
-    def value(self, val: Any):
+    def value(self, val):
         if self.is_valid(val):
             self.__value = val
             return
@@ -42,12 +42,12 @@ class Description(ABC):
         )
 
     @abstractmethod
-    def does_pass(self, actual: str | None) -> bool:
+    def does_pass(self, actual: Optional[str]) -> bool:
         pass
 
 
 class Boolean(Description):
-    def is_valid(self, value: any):
+    def is_valid(self, value):
         return isinstance(value, bool)
 
 
@@ -55,24 +55,28 @@ class Length(Description):
     def is_valid(self, value):
         return isinstance(value, int) and value > 0
 
-    def does_pass(self, actual: str | None) -> bool:
-        return hasattr(actual, '__len__') and actual.__len__() == self.value
+    def does_pass(self, actual: Optional[str]) -> bool:
+        return (
+            actual is not None
+            and hasattr(actual, "__len__")
+            and actual.__len__() == self.value
+        )
 
 
 class MinLength(Length):
-    def does_pass(self, actual: str | None) -> bool:
+    def does_pass(self, actual: Optional[str]) -> bool:
         return (
             isinstance(actual, str)
-            and hasattr(actual, '__len__')
+            and hasattr(actual, "__len__")
             and actual.__len__() >= self.value
         )
 
 
 class MaxLength(Length):
-    def does_pass(self, actual: str | None) -> bool:
+    def does_pass(self, actual: Optional[str]) -> bool:
         return (
             isinstance(actual, str)
-            and hasattr(actual, '__len__')
+            and hasattr(actual, "__len__")
             and actual.__len__() <= self.value
         )
 
@@ -85,7 +89,7 @@ class Regex(Description):
         except Exception:
             return False
 
-    def does_pass(self, actual: str | None) -> bool:
+    def does_pass(self, actual: Optional[str]) -> bool:
         return actual != None and re.match(self.value, actual)
 
 
@@ -93,11 +97,11 @@ class Option(Description):
     def is_valid(self, value: list):
         return (
             isinstance(value, list)
-            and hasattr(value, '__iter__')
+            and hasattr(value, "__iter__")
             and value.__len__() > 0
         )
 
-    def does_pass(self, actual: str | None) -> bool:
+    def does_pass(self, actual: Optional[str]) -> bool:
         return actual in self.value
 
 
@@ -109,20 +113,20 @@ class Constant(Description):
         except Exception:
             return False
 
-    def does_pass(self, actual: str | None) -> bool:
+    def does_pass(self, actual: Optional[str]) -> bool:
         return str(actual) == str(self.value)
 
 
 class IsInt(Boolean):
-    def does_pass(self, actual: any):
+    def does_pass(self, actual):
         return isinstance(actual, int) == self.value
 
 
 class IsStr(Boolean):
-    def does_pass(self, actual: any):
+    def does_pass(self, actual):
         return isinstance(actual, str) == self.value
 
 
 class IsFloat(Boolean):
-    def does_pass(self, actual: any):
+    def does_pass(self, actual):
         return isinstance(actual, float) == self.value
